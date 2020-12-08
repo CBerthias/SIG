fb5afa9d2726723448c57c913a7e71575f262a72
Corentin <cb.36@outlook.com>
2020-12-08 17:36:18 +0100

    Le scan d'un qrcode

--- app/src/main/java/com/projetetu/sig/MainActivity.java
+++ app/src/main/java/com/projetetu/sig/MainActivity.java
@@ -1,14 +1,84 @@
 package com.projetetu.sig;
 
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivityDebug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goScan(View view) {
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivityForResult(intent, 77);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 77) {
+                Log.d(TAG, "onActivityResult: ICI");
+                scanImage(data.getExtras().getParcelable("photo_result"));
+            }
+        }
+    }
+
+    private void scanImage(Uri uri) {
+        BarcodeScannerOptions options =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_QR_CODE)
+                        .build();
+
+        InputImage image = null;
+        try {
+            image = InputImage.fromFilePath(MainActivity.this, uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assert image != null;
+        BarcodeScanner scanner = BarcodeScanning.getClient(options);
+
+        Task<List<Barcode>> result = scanner.process(image)
+                .addOnSuccessListener(barcodes -> {
+                    for (Barcode barcode: barcodes) {
+                        String rawValue = barcode.getRawValue();
+                        Log.d(TAG, rawValue);
+                        Toast.makeText(this, rawValue, Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.d(TAG, "scanImage: Scan échoué");
+                });
     }
 }
